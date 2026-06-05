6e01866207de60a26df57e6a380188d5c8a66d77
nicolasrivarola-ar <nicolas.rivarola@ucsal.edu.br>
2026-06-05 13:06:04 -0300

    update src\SistemaCadastro.java
    criei a class SistemaCadastro, iniciei minha frunção

--- sistema-sadastro.java
+++
@@ -1,2 +0,0 @@
-xbdkcjncdfn
-chaca
---
+++ src/SistemaCadastro.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+	public static void main(String[] args) {
+	int num1 = 2; 
+	int num2 = 3; 
+	
+	System.out.
+	
+		}
+	}
